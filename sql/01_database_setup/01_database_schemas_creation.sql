f09ab210e4b1b7a64e4fbd5af37d32b65bc796fa
jakubm20002 <257320156+jakubm20002@users.noreply.github.com>
2026-03-11 08:56:53 +0100

    Add files via upload

---
+++ sql/01_database_setup/01_database_schemas_creation.sql
@@ -0,0 +1,19 @@
+/*
+====================================
+Creation of Databese and Schemas
+====================================
+*/
+CREATE DATABASE DataProject;
+GO
+
+USE DataProject;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold
+
